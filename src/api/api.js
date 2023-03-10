51af136f0ee3ff0cf0905464e16051599ce4063d
Kasaija Kenneth <kasaijak@gmail.com>
2023-03-10 11:56:49 +0300

    Define api actions for different endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,21 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/';
+axios.defaults.baseUrl = 'http://localhost:5000/';
+
+export const fetchAllOrders = async () => {
+  try {
+    const response = await axios.get('api/v1/foods');
+    return response.data;
+  } catch (e) {
+    throw e.toString();
+  }
+};
+
+export const fetchSingleOrder = async () => {
+  try {
+    const response = await axios.get('api/v1/foods/1');
+    return response.data;
+  } catch (e) {
+    throw e.toString();
+  }
+};
