6c01bfd88a480b303d38791e4198d9090162b7da
Kasaija Kenneth <kasaijak@gmail.com>
2023-03-10 12:01:17 +0300

    Generate Order reducer

---
+++ src/redux/reducer/OrderReducer.js
@@ -0,0 +1,25 @@
+import { Actions } from '../constants/Constants';
+
+const initiaState = {
+  orders: [],
+  order: {},
+};
+
+const orderReducer = (state = initiaState, { type, payload }) => {
+  if (type === Actions.ALL_ORDERS) {
+    return {
+      ...state,
+      orders: [payload],
+    };
+  }
+
+  if (type === Actions.SINGLE_ORDER) {
+    return {
+      ...state,
+      order: payload,
+    };
+  }
+  return state;
+};
+
+export default orderReducer;
