d85fd3d1ac8d860424696a8becab74d86e241012
Olivier-Kango <olivierkango@gmail.com>
2023-03-14 08:36:57 +0200

    Update the Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
-import singleFoodReducer from './foods/foodDetails';
 import reducerFood from './foods/foods';
 import orderReducer from './reducer/OrderReducer';
 
 const rootReducer = combineReducers({
-  // food: reducerSingleFood,
   orders: orderReducer,
-  // order: reducerSingleOrder,
   foods: reducerFood,
-  // newFood: reducerAddFood,
-  food: singleFoodReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
