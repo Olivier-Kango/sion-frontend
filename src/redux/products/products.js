d5c3a6a3ab0c9204c15139159c4a1ac14e9a88a1
Olivier-Kango <olivierkango@gmail.com>
2023-06-08 17:06:31 +0300

    Last change food to product

--- src/redux/products/products.js
+++ src/redux/products/products.js
@@ -4,31 +4,31 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://meal-master-backend.onrender.com/';
 
 // Actions
-export const getAllFoods = createAsyncThunk('GET_ALL_FOODS', async () => {
-  const response = await axios.get('api/v1/foods');
+export const getAllProducts = createAsyncThunk('GET_ALL_PRODUCTS', async () => {
+  const response = await axios.get('api/v1/products');
   return response.data;
 });
 
-export const addFood = createAsyncThunk('ADD_FOOD', async (food) => {
-  const response = await axios.post('api/v1/foods', food);
+export const addProduct = createAsyncThunk('ADD_PRODUCT', async (product) => {
+  const response = await axios.post('api/v1/products', product);
   return response.data;
 });
 
-export const deleteFood = createAsyncThunk('DELETE_FOOD', async (id) => {
-  await axios.delete(`api/v1/foods/${id}`);
+export const deleteProduct = createAsyncThunk('DELETE_PRODUCT', async (id) => {
+  await axios.delete(`api/v1/products/${id}`);
   return id;
 });
 
 // Reducers
-const reducerFood = (state = [], action) => {
+const reducerProduct = (state = [], action) => {
   switch (action.type) {
-    case 'GET_ALL_FOODS/fulfilled': {
+    case 'GET_ALL_PRODUCTS/fulfilled': {
       return action.payload;
     }
-    case 'ADD_FOOD/fulfilled': {
+    case 'ADD_PRODUCT/fulfilled': {
       return [...state, action.payload];
     }
-    case 'DELETE_FOOD/fulfilled': {
+    case 'DELETE_PRODUCT/fulfilled': {
       return state.filter((f) => f.id !== action.payload);
     }
     default:
@@ -36,4 +36,4 @@ const reducerFood = (state = [], action) => {
   }
 };
 
-export default reducerFood;
+export default reducerProduct;
