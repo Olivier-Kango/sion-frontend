59ba783b02c169a7fed6d730140601700d761e73
Olivier-Kango <olivierkango@gmail.com>
2023-07-06 20:41:15 +0300

    Logout to login

--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -45,7 +45,7 @@ const LeftBar = ({ open, handleLinkClick, isAuthenticated }) => {
 
   const handleLogout = () => {
     dispatch(userLogout());
-    navigate('/');
+    navigate('/login-page');
   };
 
   const handleAddProductClick = () => {
