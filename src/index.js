e48eec12ef6525cdeecaf6aaf39ca9741055065b
Olivier-Kango <olivierkango@gmail.com>
2023-07-11 21:02:47 +0300

    comment api

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 // Redux store
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
 import store from './redux/store';
 
 import './index.scss';
