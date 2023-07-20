8392e0a3bdf6da2cf5f09a55ffb16a663bd4fd3a
Olivier-Kango <olivierkango@gmail.com>
2023-07-20 13:15:46 +0300

    Fix linter error

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Footer = () => (
-  <div className="footer">
-    © 2023 Olivier Kango
-  </div>
-);
+const Footer = () => {
+  // Obtenir l'année actuelle
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="footer">
+      ©
+      {' '}
+      {currentYear}
+      {' '}
+      Olivier Kango
+    </div>
+  );
+};
 
 export default Footer;
