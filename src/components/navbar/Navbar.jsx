8396093ae574f950ca4b31856075e8abf0e22f94
Olivier-Kango <olivierkango@gmail.com>
2023-08-24 16:02:27 +0300

    Add logout for user

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
   faSearch,
   faFolderOpen,
@@ -9,13 +9,21 @@ import {
   faBriefcase,
 } from '@fortawesome/free-solid-svg-icons';
 import profilePic from '../../assets/profile-pic.jpeg';
+import { userLogout } from '../../redux/users/users';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const user = useSelector((state) => state.user);
   const usern = useSelector((state) => state.user.data?.name);
   const username = usern?.charAt(0).toUpperCase() + usern?.slice(1);
   const isAuthenticated = user.loggedIn;
+
+  const handleLogout = () => {
+    dispatch(userLogout());
+    navigate('/login-page');
+  };
 
   return (
     <div className="navbar">
@@ -49,6 +57,15 @@ const Navbar = () => {
           <div className="navbar-profile navbar-link">
             <img src={profilePic} alt="Profile" />
             <span className="text">{`Hello, ${username}`}</span>
+            <div className="popup">
+              <button
+                type="button"
+                className="item logout"
+                onClick={() => handleLogout()}
+              >
+                <span className="text">Logout</span>
+              </button>
+            </div>
           </div>
         ) : (
           <Link to="/login-page" style={{ textDecoration: 'none' }}>
