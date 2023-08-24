531cdf5acecd0ce4ec87f76fdffd1dc06718da4a
Olivier-Kango <olivierkango@gmail.com>
2023-08-24 03:44:27 +0300

    Add nav first and Nav second

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,38 +17,42 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <div className="navbar-header">
-        <Link to="/" style={{ textDecoration: 'none' }}>
-          <div className="logo">PSS Digital</div>
-        </Link>
-      </div>
-      <div className="navbar-search">
-        <div className="search-icon">
-          <FontAwesomeIcon icon={faSearch} />
+      <div className="nav-first">
+        <div className="navbar-header">
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <div className="logo">PSS Digital</div>
+          </Link>
         </div>
-        <input type="text" placeholder="Search for a product" />
-      </div>
-      <div className="navbar-links">
-        <div className="navbar-link">
-          <FontAwesomeIcon icon={faFolderOpen} className="nav-icon" />
-          <span className="text">Portfolio</span>
-        </div>
-        <div className="navbar-link">
-          <FontAwesomeIcon icon={faShoppingCart} className="nav-icon" />
-          <span className="text">E-commerce</span>
-        </div>
-        <div className="navbar-link">
-          <FontAwesomeIcon icon={faBriefcase} className="nav-icon" />
-          <span className="text">B-Management</span>
+        <div className="navbar-search">
+          <div className="search-icon">
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
+          <input type="text" placeholder="Search for a product" />
         </div>
       </div>
-      <div className="navbar-profile">
-        <img src={profilePic} alt="Profile" />
-        <span className="text">
-          Hello,
-          {' '}
-          {Number.isNaN(username) ? 'Sign In' : username}
-        </span>
+      <div className="nav-second">
+        <div className="navbar-links">
+          <div className="navbar-link">
+            <FontAwesomeIcon icon={faFolderOpen} className="nav-icon" />
+            <span className="text">Portfolio</span>
+          </div>
+          <div className="navbar-link">
+            <FontAwesomeIcon icon={faShoppingCart} className="nav-icon" />
+            <span className="text">E-commerce</span>
+          </div>
+          <div className="navbar-link">
+            <FontAwesomeIcon icon={faBriefcase} className="nav-icon" />
+            <span className="text">B-Management</span>
+          </div>
+        </div>
+        <div className="navbar-profile">
+          <img src={profilePic} alt="Profile" />
+          <span className="text">
+            Hello,
+            {' '}
+            {Number.isNaN(username) ? 'Sign In' : username}
+          </span>
+        </div>
       </div>
     </div>
   );
