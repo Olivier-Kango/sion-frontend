5dacf0b8fa5d9a11cc1047210fe2b23768431016
Olivier-Kango <olivierkango@gmail.com>
2023-09-07 07:43:28 +0300

    Harware Store in front

--- src/components/product/AddProduct.jsx
+++ src/components/product/AddProduct.jsx
@@ -131,13 +131,13 @@ const AddProduct = () => {
                 className="category-select"
               >
                 <option value="" className="placeholder-option">Select product&apos;s Category</option>
+                <option value="Hardware Store">Hardware Store</option>
                 <option value="Mineral Water">Mineral Water</option>
                 <option value="Beverage Store">Beverage Store</option>
                 <option value="Electronics">Electronics</option>
                 <option value="IT Services">IT Services</option>
                 <option value="Gas Energy">Gas Energy</option>
                 <option value="Real Estate">Real Estate</option>
-                <option value="Hardware Store">Hardware Store</option>
                 <option value="Chickens">Chickens</option>
               </select>
             </div>
