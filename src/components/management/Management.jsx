523dabe5c5debbcb87dc251e097b9bf1afbacbbe
Olivier-Kango <olivierkango@gmail.com>
2023-10-09 20:51:50 +0300

    Add table

--- src/components/management/Management.jsx
+++ src/components/management/Management.jsx
@@ -6,12 +6,46 @@ import './Management.scss';
 const Management = () => {
   const user = useSelector((state) => state.user.data);
   const isAuthenticated = useSelector((state) => state.user.loggedIn);
+  const products = useSelector((state) => state.products.products);
 
   return (
     <div className="project-management">
       {user.role === 'admin' ? (
-        <div className="pencil-trash">
-          Welcome Admin
+        <div className="management">
+          {products.length}
+          <br />
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Image</th>
+                <th>Unit Price</th>
+                <th>Quantity</th>
+                <th>Category</th>
+                <th>Unit Purchase Price</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+                <th>Subcategory</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product.id}>
+                  <td>{product.id}</td>
+                  <td>{product.name}</td>
+                  <td><img src={product.image} alt={product.name} /></td>
+                  <td>{product.unit_price}</td>
+                  <td>{product.quantity}</td>
+                  <td>{product.category}</td>
+                  <td>{product.unit_purchase_price}</td>
+                  <td>{product.created_at}</td>
+                  <td>{product.updated_at}</td>
+                  <td>{product.subcategory}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       ) : (
         <>
@@ -23,7 +57,7 @@ const Management = () => {
             <br />
             Please
             {' '}
-            <Link to={isAuthenticated && '/login-page'} className="login-manager">
+            <Link to={!isAuthenticated && '/login-page'} className="login-manager">
               click here
             </Link>
             {' '}
