9c475a1d93dceb260d925a1b1d671e2626371048
Olivier-Kango <olivierkango@gmail.com>
2023-10-09 20:19:16 +0300

    Add accessing management

--- src/components/management/Management.jsx
+++ src/components/management/Management.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './Management.scss';
 
-const Management = () => (
-  <div className="project-management">
-    <h1>
-      Business Management under construction
-    </h1>
-    <p>We&apos;re currently working on bringing you an amazing website.</p>
-    <p>Stay tuned for exciting updates and content!</p>
-  </div>
-);
+const Management = () => {
+  const user = useSelector((state) => state.user.data);
+  const isAuthenticated = useSelector((state) => state.user.loggedIn);
+
+  return (
+    <div className="project-management">
+      {user.role === 'admin' ? (
+        <div className="pencil-trash">
+          Welcome Admin
+        </div>
+      ) : (
+        <>
+          <h1>
+            Accessing Management Details
+          </h1>
+          <p>
+            To access management details, you need to be an admin. Please
+            <Link to={isAuthenticated && '/login-page'} className="login-manager">
+              click here
+            </Link>
+            to log in
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Management;
