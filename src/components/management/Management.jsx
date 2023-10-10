ab1a2864738ba45406fa86dc5ea96c008126da20
Olivier-Kango <olivierkango@gmail.com>
2023-10-10 20:41:27 +0300

    Update margin

--- src/components/management/Management.jsx
+++ src/components/management/Management.jsx
@@ -28,28 +28,28 @@ const Management = () => {
             <thead>
               <tr>
                 <th>N°</th>
-                <th>Name</th>
-                <th>Image</th>
-                <th>P.A.</th>
-                <th>P.V.</th>
-                <th>Gain</th>
-                <th>Quantity</th>
-                <th>Category</th>
-                <th>Subcategory</th>
+                <th className="name">Name</th>
+                <th className="td-img">Img</th>
+                <th className="prix">P.A.</th>
+                <th className="prix">P.V.</th>
+                <th className="prix">B</th>
+                <th className="quantity">Qté</th>
+                <th className="cat">Category</th>
+                <th className="cat">Subcategory</th>
               </tr>
             </thead>
             <tbody>
               {sortedProducts.map((product, index) => (
                 <tr key={product.id}>
                   <td>{index + 1}</td>
-                  <td>{product.name}</td>
+                  <td className="name">{product.name}</td>
                   <td className="td-img"><img src={product.image} alt={product.name} /></td>
-                  <td>{product.unit_purchase_price}</td>
-                  <td>{product.unit_price}</td>
-                  <td>{(product.unit_price - product.unit_purchase_price).toFixed(2)}</td>
-                  <td>{product.quantity}</td>
-                  <td>{product.category}</td>
-                  <td>{product.subcategory}</td>
+                  <td className="prix">{product.unit_purchase_price}</td>
+                  <td className="prix">{product.unit_price}</td>
+                  <td className="prix">{(product.unit_price - product.unit_purchase_price).toFixed(2)}</td>
+                  <td className="quantity">{product.quantity}</td>
+                  <td className="cat">{product.category}</td>
+                  <td className="cat">{product.subcategory}</td>
                 </tr>
               ))}
             </tbody>
