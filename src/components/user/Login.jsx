3599857626cfb32bba06e8d782868888f7ebd621
Olivier-Kango <olivierkango@gmail.com>
2023-12-09 16:12:09 +0200

    Add some styling for the Button

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import { userLogin } from '../../redux/users/users';
 import './user.scss';
+import './Button.scss';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -15,8 +16,8 @@ const Login = () => {
 
   const state = 'default'; // default, loading, disabled
   const text = 'Login';
-  const showText = true;
-  const showIcon = false;
+  let showText = true;
+  let showIcon = false;
   const size = 'medium'; // medium | large | small | icon
   const variant = 'primary'; // 'primary' | 'secondary' | 'subtle' | text
 
@@ -51,8 +52,8 @@ const Login = () => {
   // };
 
   if (state === 'loading') {
-    // const showIcon = true;
-    // const showText = !showIcon;
+    showIcon = true;
+    showText = !showIcon;
   }
 
   const handleSubmit = (e) => {
@@ -122,26 +123,12 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className={`group relative flex w-1/3 justify-center mt-3 rounded-md py-2 px-3 text-xl font-semibold text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 button ${dynamicState} ${dynamicSize} ${dynamicVariant} ${showIcon ? 'disableInteractions' : ''}`}
+            className={`button ${dynamicState} ${dynamicSize} ${dynamicVariant} ${showIcon ? 'disableInteractions' : ''}`}
             disabled={state === 'disabled' || state === 'loading'}
             // onClick={handleClick}
             // onBlur={handleBlur}
           >
-            {showIcon && (
-              (
-                (variant === 'secondary' || variant === 'subtle') && size !== 'icon'
-              )
-                || (
-                  variant === 'text' && size === 'icon'
-                )
-            ) && (
-              // <Image
-              //   src={icon}
-              //   alt="Button Icon"
-              //   width={size === 'small' ? 16 : 24}
-              //   height={size === 'small' ? 16 : 24}
-              //   className={styles.icon}
-              // />
+            {showIcon && ((variant === 'primary') || (variant === 'subtle' && size === 'icon')) && (
               <svg
                 width={size === 'small' ? 16 : 24}
                 height={size === 'small' ? 16 : 24}
@@ -159,19 +146,8 @@ const Login = () => {
                 />
               </svg>
             )}
-            {showIcon && (
-              (
-                (variant === 'secondary' || variant === 'subtle') && size !== 'icon'
-              )
-                || (variant === 'text' && size === 'icon')
-            ) && (
-              // <Image
-              //   src="/LoadingG.svg"
-              //   alt="Button Icon"
-              //   width={24}
-              //   height={24}
-              //   className={styles.iconG}
-              // />
+
+            {showIcon && (variant === 'secondary' || (variant === 'subtle' && size !== 'icon') || (variant === 'text' && size === 'icon')) && (
               <svg
                 width="24"
                 height="24"
@@ -183,10 +159,12 @@ const Login = () => {
                 <path d="M9.99967 1.66658C8.3515 1.66658 6.74033 2.15533 5.36992 3.071C3.99951 3.98668 2.93141 5.28817 2.30068 6.81089C1.66995 8.33361 1.50492 10.0092 1.82646 11.6257C2.14801 13.2422 2.94168 14.727 4.10712 15.8925C5.27255 17.0579 6.75741 17.8516 8.37392 18.1731C9.99043 18.4947 11.666 18.3296 13.1887 17.6989C14.7114 17.0682 16.0129 16.0001 16.9286 14.6297C17.8443 13.2593 18.333 11.6481 18.333 9.99992" stroke="#60C1C6" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
             )}
+
             {showIcon
               && (variant === 'text' && size !== 'icon') && (
                 <span>Loading</span>
             )}
+
             {!showText && size === 'icon' && state !== 'loading' && (
               <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="iconForward">
                 <path d="M1 1L7.96317 7.96317L1 14.9263" stroke="" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
