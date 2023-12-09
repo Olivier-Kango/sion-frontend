d5dfc20961e51d5b70f49af16ddb645eb23e1753
Olivier-Kango <olivierkango@gmail.com>
2023-12-09 12:53:54 +0200

    Fix errors in button

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -13,6 +13,48 @@ const Login = () => {
   const navigate = useNavigate();
   const formRef = useRef();
 
+  const state = 'default'; // default, loading, disabled
+  const text = 'Login';
+  const showText = true;
+  const showIcon = false;
+  const size = 'medium'; // medium | large | small | icon
+  const variant = 'primary'; // 'primary' | 'secondary' | 'subtle' | text
+
+  let dynamicState = '';
+
+  if (state === 'disabled') {
+    dynamicState = 'disabled';
+  } else if (state === 'loading') {
+    dynamicState = 'loading';
+  }
+
+  let dynamicSize = '';
+
+  if (size === 'large') {
+    dynamicSize = 'largeSize';
+  } else if (size === 'icon') {
+    dynamicSize = 'iconSize';
+  } else if (size === 'small') {
+    dynamicSize = 'smallSize';
+  }
+
+  const dynamicVariant = variant;
+  // const [isActive, setIsActive] = useState(false);
+
+  // const handleClick = () => {
+  //   setIsActive(true);
+  //   // Add your other click logic here
+  // };
+
+  // const handleBlur = () => {
+  //   setIsActive(false);
+  // };
+
+  if (state === 'loading') {
+    // const showIcon = true;
+    // const showText = !showIcon;
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(formRef.current);
@@ -80,9 +122,77 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="group relative flex w-1/3 justify-center mt-3 rounded-md py-2 px-3 text-xl font-semibold text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className={`group relative flex w-1/3 justify-center mt-3 rounded-md py-2 px-3 text-xl font-semibold text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 button ${dynamicState} ${dynamicSize} ${dynamicVariant} ${showIcon ? 'disableInteractions' : ''}`}
+            disabled={state === 'disabled' || state === 'loading'}
+            // onClick={handleClick}
+            // onBlur={handleBlur}
           >
-            Login
+            {showIcon && (
+              (
+                (variant === 'secondary' || variant === 'subtle') && size !== 'icon'
+              )
+                || (
+                  variant === 'text' && size === 'icon'
+                )
+            ) && (
+              // <Image
+              //   src={icon}
+              //   alt="Button Icon"
+              //   width={size === 'small' ? 16 : 24}
+              //   height={size === 'small' ? 16 : 24}
+              //   className={styles.icon}
+              // />
+              <svg
+                width={size === 'small' ? 16 : 24}
+                height={size === 'small' ? 16 : 24}
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon"
+              >
+                <path
+                  d="M9.99967 1.66658C8.3515 1.66658 6.74033 2.15533 5.36992 3.071C3.99951 3.98668 2.93141 5.28817 2.30068 6.81089C1.66995 8.33361 1.50492 10.0092 1.82646 11.6257C2.14801 13.2422 2.94168 14.727 4.10712 15.8925C5.27255 17.0579 6.75741 17.8516 8.37392 18.1731C9.99043 18.4947 11.666 18.3296 13.1887 17.6989C14.7114 17.0682 16.0129 16.0001 16.9286 14.6297C17.8443 13.2593 18.333 11.6481 18.333 9.99992"
+                  stroke="#FCFCFC"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            )}
+            {showIcon && (
+              (
+                (variant === 'secondary' || variant === 'subtle') && size !== 'icon'
+              )
+                || (variant === 'text' && size === 'icon')
+            ) && (
+              // <Image
+              //   src="/LoadingG.svg"
+              //   alt="Button Icon"
+              //   width={24}
+              //   height={24}
+              //   className={styles.iconG}
+              // />
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="iconG"
+              >
+                <path d="M9.99967 1.66658C8.3515 1.66658 6.74033 2.15533 5.36992 3.071C3.99951 3.98668 2.93141 5.28817 2.30068 6.81089C1.66995 8.33361 1.50492 10.0092 1.82646 11.6257C2.14801 13.2422 2.94168 14.727 4.10712 15.8925C5.27255 17.0579 6.75741 17.8516 8.37392 18.1731C9.99043 18.4947 11.666 18.3296 13.1887 17.6989C14.7114 17.0682 16.0129 16.0001 16.9286 14.6297C17.8443 13.2593 18.333 11.6481 18.333 9.99992" stroke="#60C1C6" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            )}
+            {showIcon
+              && (variant === 'text' && size !== 'icon') && (
+                <span>Loading</span>
+            )}
+            {!showText && size === 'icon' && state !== 'loading' && (
+              <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="iconForward">
+                <path d="M1 1L7.96317 7.96317L1 14.9263" stroke="" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            )}
+            {showText && size !== 'icon' && text}
           </button>
         </form>
 
