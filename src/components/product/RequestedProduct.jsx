41bad55c195ff83bdaeb0823687b861652d3a021
Olivier-Kango <olivierkango@gmail.com>
2024-01-17 17:07:28 +0200

    Add loading message

--- src/components/product/RequestedProduct.jsx
+++ src/components/product/RequestedProduct.jsx
@@ -11,7 +11,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // imports icons for react-icons library
 import { LuSendHorizonal } from 'react-icons/lu';
-import { FaUndo, FaTrash } from 'react-icons/fa';
+import { FaUndo, FaTrash, FaReact } from 'react-icons/fa';
 import { MdMoreVert } from 'react-icons/md';
 
 // import actoins for requested products
@@ -34,15 +34,43 @@ const RequestedProduct = () => {
   // Redux state for requested products
   const requestedProducts = useSelector((state) => state.requestedProducts.requestedProducts);
 
+  // local states
+  const [openPopupId, setOpenPopupId] = useState(null);
+  const [localRequestCount, setLocalRequestCount] = useState(0);
+  const [highlightedProductId, setHighlightedProductId] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Memoize the sorted and mapped requested products
+  const sortedAndMappedProducts = useMemo(() => (requestedProducts
+    ? requestedProducts
+      .slice() // create a copy to avoid mutating the original array
+      .sort((a, b) => b.request_count - a.request_count)
+      .map((product) => ({
+        id: product.id,
+        name: product.name,
+        request_count: product.request_count,
+      }))
+    : []), [requestedProducts]);
+
   const dispatch = useDispatch();
 
   // Fetch all requested products on component mount
   useEffect(() => {
-    dispatch(getAllRequestedProducts());
-  }, [dispatch]);
+    setLoading(true); // Commence en état de chargement
 
-  // State for managing popup for reset and delete
-  const [openPopupId, setOpenPopupId] = useState(null);
+    dispatch(getAllRequestedProducts())
+      .then((action) => {
+        if (action.type === 'GET_ALL_REQUESTED_PRODUCTS/fulfilled') {
+          // La requête a réussi, donc on met loading à false
+          setLoading(false);
+        }
+      })
+      .catch((error) => {
+        // La requête a échoué, donc on met loading à true
+        console.error('Error loading data:', error);
+        setLoading(true);
+      });
+  }, [dispatch]);
 
   // Toggle the visibility of the popup for a specific product
   const handleTogglePopup = (productId) => {
@@ -51,11 +79,21 @@ const RequestedProduct = () => {
 
   // Increment the request count for a specific product
   const handleIncrementRequest = (productId, currentRequestCount) => {
+    // Update the local count immediately for a smooth user experience
+    setLocalRequestCount((prevCounts) => ({ ...prevCounts, [productId]: currentRequestCount + 1 }));
+
     const updatedRequestedProductData = {
       request_count: currentRequestCount + 1,
     };
 
     dispatch(incrementRequestCount({ id: productId, updatedRequestedProductData }));
+
+    // update class for smooth sorting
+    setHighlightedProductId(productId);
+
+    setTimeout(() => {
+      setHighlightedProductId(null);
+    }, 1000);
   };
 
   // Reset the request count for a specific product
@@ -65,6 +103,11 @@ const RequestedProduct = () => {
     };
 
     dispatch(resetRequestCount({ id: productId, updatedRequestedProductData }));
+    requestedProductsRef.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
   };
 
   // Delete a requested product
@@ -131,54 +174,61 @@ const RequestedProduct = () => {
       {/* Title */}
       <h1 className="title">Requested Products</h1>
 
+      {loading && (
+        <div className="loading-container">
+          <FaReact className="loading-icon" />
+          <p className="loading-message slide-in-out">Loading...</p>
+        </div>
+      )}
       {/* Product list */}
       <ul className="product-list">
-        {requestedProducts && requestedProducts
-          .sort((a, b) => b.request_count - a.request_count)
-          .map((product) => (
-            <li key={product.id} className="product-entry">
-              <span>{product.name}</span>
+        {sortedAndMappedProducts.map((product) => (
+          <li
+            key={product.id}
+            className={`product-entry ${highlightedProductId === product.id ? 'highlighted' : ''}`}
+          >
+            <span>{product.name}</span>
 
-              {/* Popup for reset and delete options */}
-              <div className="popup" style={{ visibility: openPopupId === product.id ? 'visible' : 'hidden' }}>
-                <svg aria-hidden="true" height="12" viewBox="0 0 25 12" width="25" className="x10l6tqk xng853d xu8u0ou" fill="white" style={{ transform: 'scale(1, -1) translate(0px, 0px) rotateY(180deg)' }}>
-                  <path d="M24.553.103c-2.791.32-5.922 1.53-7.78 3.455l-9.62 7.023c-2.45 2.54-5.78 1.645-5.78-2.487V2.085C1.373 1.191.846.422.1.102h24.453z" />
-                </svg>
+            {/* Popup for reset and delete options */}
+            <div className="popup" style={{ visibility: openPopupId === product.id ? 'visible' : 'hidden' }}>
+              <svg aria-hidden="true" height="12" viewBox="0 0 25 12" width="25" className="x10l6tqk xng853d xu8u0ou" fill="white" style={{ transform: 'scale(1, -1) translate(0px, 0px) rotateY(180deg)' }}>
+                <path d="M24.553.103c-2.791.32-5.922 1.53-7.78 3.455l-9.62 7.023c-2.45 2.54-5.78 1.645-5.78-2.487V2.085C1.373 1.191.846.422.1.102h24.453z" />
+              </svg>
 
-                {/* Buttons for reset and delete actions */}
-                <div className="delete-reset">
-                  <button
-                    type="button"
-                    onClick={() => handleResetRequest(product.id)}
-                  >
-                    <FaUndo className="icon" />
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => handleDeleteRequestedProduct(product.id)}
-                  >
-                    <FaTrash className="icon" />
-                  </button>
-                </div>
+              {/* Buttons for reset and delete actions */}
+              <div className="delete-reset">
+                <button
+                  type="button"
+                  onClick={() => handleResetRequest(product.id)}
+                >
+                  <FaUndo className="icon" />
+                </button>
+                <button
+                  type="button"
+                  onClick={() => handleDeleteRequestedProduct(product.id)}
+                >
+                  <FaTrash className="icon" />
+                </button>
               </div>
+            </div>
 
-              {/* 3 dots icon for reset and delete actions */}
-              <MdMoreVert
-                onClick={() => handleTogglePopup(product.id)}
-                className="ellipsis"
-              />
+            {/* 3 dots icon for reset and delete actions */}
+            <MdMoreVert
+              onClick={() => handleTogglePopup(product.id)}
+              className="ellipsis"
+            />
 
-              {/* Button for incrementing request count */}
-              <button
-                type="button"
-                onClick={() => handleIncrementRequest(product.id, product.request_count)}
-                className="button-like"
-              >
-                <img className="icon" src="https://scontent.fkgl4-1.fna.fbcdn.net/m1/v/t6/An_Hu2MGghXfWhrGQLADBvMqHBUxBoVMkVyPd6nn5lnsrwR-vi4BbkvRAbUlxUY9vGSt_yQiOgk2XFidRDZtah01ve6N3Ln9ICuzKj0ZRWl7nKjEJUNFh5EMkRfQa4lMXQ.png?ccb=10-5&amp;oh=00_AfA29E8TGGwFzpSbPtpaDfTXHa_V0tMYVQ_HiZmf1QaAOA&amp;oe=65CE93EC&amp;_nc_sid=7da55a" alt="" style={{ height: '20px', width: '20px' }} />
-                <span>{product.request_count}</span>
-              </button>
-            </li>
-          ))}
+            {/* Button for incrementing request count */}
+            <button
+              type="button"
+              onClick={() => handleIncrementRequest(product.id, product.request_count)}
+              className="button-like"
+            >
+              <img className="icon" src="https://scontent.fkgl4-1.fna.fbcdn.net/m1/v/t6/An_Hu2MGghXfWhrGQLADBvMqHBUxBoVMkVyPd6nn5lnsrwR-vi4BbkvRAbUlxUY9vGSt_yQiOgk2XFidRDZtah01ve6N3Ln9ICuzKj0ZRWl7nKjEJUNFh5EMkRfQa4lMXQ.png?ccb=10-5&amp;oh=00_AfA29E8TGGwFzpSbPtpaDfTXHa_V0tMYVQ_HiZmf1QaAOA&amp;oe=65CE93EC&amp;_nc_sid=7da55a" alt="" style={{ height: '20px', width: '20px' }} />
+              <span>{localRequestCount[product.id] || product.request_count}</span>
+            </button>
+          </li>
+        ))}
       </ul>
 
       {/* Form for adding requested products */}
