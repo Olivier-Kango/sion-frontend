df87ce576b015b87405669387986b400c89705ce
Olivier-Kango <olivierkango@gmail.com>
2024-01-17 02:30:44 +0200

    Add vertical 3 dots

--- src/components/product/RequestedProduct.jsx
+++ src/components/product/RequestedProduct.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { LuSendHorizonal } from 'react-icons/lu';
+import { FaUndo, FaTrash } from 'react-icons/fa';
+import { MdMoreVert } from 'react-icons/md';
 import {
   getAllRequestedProducts,
   incrementRequestCount,
@@ -42,6 +44,7 @@ const RequestedProduct = () => {
   const [name, setname] = useState('');
   const [requestCount] = useState(1);
   const [showButton, setShowButton] = useState(false);
+  // const [showPopup, setShowPopup] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -56,7 +59,6 @@ const RequestedProduct = () => {
           type: 'ADD_REQUESTED_PRODUCT/fulfilled',
           payload: action.payload,
         });
-        console.log(requestedProductsRef.current);
         requestedProductsRef.current?.scrollIntoView({
           behavior: 'smooth',
           block: 'start',
@@ -70,39 +72,57 @@ const RequestedProduct = () => {
     setShowButton(name.length > 0);
   }, [name]);
 
+  // const togglePopup = (productId) => {
+  //   setShowPopup((prev) => (prev === productId ? null : productId));
+  // };
+
   return (
     <div className="container">
-      <div className="container-products">
+      <h1 className="title">Requested Products</h1>
+      <ul className="product-list">
         {requestedProducts && requestedProducts
           .sort((a, b) => b.request_count - a.request_count)
           .map((product) => (
-            <h1 key={product.id}>
+            <li key={product.id} className="product-entry">
               <span>{product.name}</span>
-              {' '}
-              <button
-                type="button"
-                onClick={() => handleResetRequest(product.id)}
-              >
-                reset
-              </button>
-              {' '}
+              <div className="popup">
+                <svg aria-hidden="true" height="12" viewBox="0 0 25 12" width="25" className="x10l6tqk xng853d xu8u0ou" fill="white" style={{ transform: 'scale(1, -1) translate(0px, 0px)' }}>
+                  <path d="M24.553.103c-2.791.32-5.922 1.53-7.78 3.455l-9.62 7.023c-2.45 2.54-5.78 1.645-5.78-2.487V2.085C1.373 1.191.846.422.1.102h24.453z" />
+                </svg>
+                <div className="delete-reset">
+                  <button
+                    type="button"
+                    onClick={() => handleResetRequest(product.id)}
+                  >
+                    <span>
+                      Reset
+                    </span>
+                    <FaUndo className="icon" />
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleDeleteRequestedProduct(product.id)}
+                  >
+                    <span>
+                      Delete
+                    </span>
+                    <FaTrash className="icon" />
+                  </button>
+                </div>
+              </div>
+              {/* <FaEllipsisH onClick={() => togglePopup(product.id)} className="ellipsis" /> */}
+              <MdMoreVert className="ellipsis" />
               <button
                 type="button"
                 onClick={() => handleIncrementRequest(product.id, product.request_count)}
+                className="button-like"
               >
-                Increment
+                <img className="icon" src="https://scontent.fkgl4-1.fna.fbcdn.net/m1/v/t6/An_Hu2MGghXfWhrGQLADBvMqHBUxBoVMkVyPd6nn5lnsrwR-vi4BbkvRAbUlxUY9vGSt_yQiOgk2XFidRDZtah01ve6N3Ln9ICuzKj0ZRWl7nKjEJUNFh5EMkRfQa4lMXQ.png?ccb=10-5&amp;oh=00_AfA29E8TGGwFzpSbPtpaDfTXHa_V0tMYVQ_HiZmf1QaAOA&amp;oe=65CE93EC&amp;_nc_sid=7da55a" alt="" style={{ height: '20px', width: '20px' }} />
+                <span>{product.request_count}</span>
               </button>
-              <button
-                type="button"
-                onClick={() => handleDeleteRequestedProduct(product.id)}
-              >
-                DELETE
-              </button>
-              {' '}
-              {product.request_count}
-            </h1>
+            </li>
           ))}
-      </div>
+      </ul>
       <form onSubmit={handleSubmit}>
         <div>
           <input
