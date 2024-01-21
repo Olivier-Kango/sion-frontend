1cd9243e960df8c13f56b427c6354a7f9d4c1904
Olivier-Kango <olivierkango@gmail.com>
2024-01-21 09:14:01 +0200

    zoom In on popup appearence

--- src/components/product/RequestedProduct.jsx
+++ src/components/product/RequestedProduct.jsx
@@ -234,7 +234,7 @@ const RequestedProduct = () => {
                   <span>{product.name}</span>
 
                   {/* Popup for reset and delete options */}
-                  <div className="popup" style={{ visibility: openPopupId === product.id ? 'visible' : 'hidden' }}>
+                  <div className={`popup ${openPopupId === product.id ? 'visible' : 'hidden'}`}>
                     <svg aria-hidden="true" height="12" viewBox="0 0 25 12" width="25" className="x10l6tqk xng853d xu8u0ou" fill="white" style={{ transform: 'scale(1, -1) translate(0px, 0px) rotateY(180deg)' }}>
                       <path d="M24.553.103c-2.791.32-5.922 1.53-7.78 3.455l-9.62 7.023c-2.45 2.54-5.78 1.645-5.78-2.487V2.085C1.373 1.191.846.422.1.102h24.453z" />
                     </svg>
