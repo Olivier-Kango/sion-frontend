d9340127598704fd601c33f82b855c7cda820713
Olivier-Kango <olivierkango@gmail.com>
2024-02-12 21:59:37 +0200

    Add 2 slides

--- src/components/slides/Slides.jsx
+++ src/components/slides/Slides.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { useMediaQuery } from '@mui/material';
 import sion from '../../assets/pub-sion.jpg';
+import coussin from '../../assets/coussin.jpg';
+import elec from '../../assets/elec.jpg';
 import gaz from '../../assets/gaz.jpg';
 import sionPhone from '../../assets/sion-phone.jpg';
 import gazPhone from '../../assets/gaz-phone.jpg';
@@ -25,6 +27,20 @@ const Slides = () => {
           className="d-block w-100"
           src={isMobile ? sionPhone : sion}
           alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={coussin}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={elec}
+          alt="Fourth slide"
         />
       </Carousel.Item>
     </Carousel>
