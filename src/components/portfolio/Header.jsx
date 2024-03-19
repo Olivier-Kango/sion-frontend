dc09c98ea8066690706208a1596f62a518fa55fb
Olivier-Kango <olivierkango@gmail.com>
2024-03-19 13:38:16 +0200

    Add Header component

---
+++ src/components/portfolio/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => (
+  <div>
+    Header
+  </div>
+);
+
+export default Header;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './Header';
 
 const Portfolio = () => (
   <div>
-    Coming Soon...
+    <Header />
   </div>
 );
 
