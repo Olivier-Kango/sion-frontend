887805b64ea35cf209d2e04daf789f10268cf0b8
Olivier-Kango <olivierkango@gmail.com>
2024-03-25 20:52:57 +0200

    Add color orange for hover

--- src/components/footer/FooterHome.jsx
+++ src/components/footer/FooterHome.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  FaGithub,
+  FaLinkedin,
+  FaAngellist,
+  FaTwitter,
+} from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
 import './FooterHome.scss';
 
 const FooterHome = () => {
@@ -63,6 +70,59 @@ const FooterHome = () => {
       <br />
       <br />
       <br />
+      <div className="social-icone" id="social">
+        <nav className="flex-nav">
+          <ul>
+            <li className="social">
+              <a
+                href="https://github.com/Olivier-Kango"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub style={{ color: '#4078c0' }} />
+              </a>
+            </li>
+            <li className="social">
+              <a
+                href="https://www.linkedin.com/in/olivier-kango/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedin style={{ color: '#0077b5' }} />
+              </a>
+            </li>
+            <li className="social">
+              <a
+                href="https://angel.co/u/kango-olivier"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaAngellist style={{ color: '#1c408c' }} />
+              </a>
+            </li>
+            <li className="social">
+              <a
+                href="https://twitter.com/olivierkango1"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaTwitter style={{ color: '#1da1f2' }} />
+              </a>
+            </li>
+            <li className="social">
+              <a
+                href="mailto:olivierkango@gmail.com"
+                id="m"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <strong><AiOutlineMail style={{ color: '#db4437' }} /></strong>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <p id="bottom">olivierkango@gmail.com</p>
       {' '}
       {currentYear}
       {' '}
